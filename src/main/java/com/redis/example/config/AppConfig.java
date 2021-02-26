115302b07756bc242f00b610cb534a780e7c301f
RV0718 <ravi.sharma0790@gmail.com>
2021-02-26 22:57:42 +0000

    Added ReadMe.md and added connectionpooling feature

--- src/main/java/com/redis/example/config/AppConfig.java
+++ src/main/java/com/redis/example/config/AppConfig.java
@@ -1,26 +1,62 @@
 package com.redis.example.config;
 
 import com.redis.example.model.Books;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class AppConfig {
 
-    @Value("${redis.hostname:localhost}")
-    private String redisHostname;
+    //    @Value("${redis.hostname:localhost}")
+    @Value("${spring.redis.host}")
+    private String redisHost;
 
-    @Value("${redis.port:6379}")
-    private Integer port;
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.jedis.pool.max-total}")
+    private int maxTotal;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+
+    @Bean
+    public JedisClientConfiguration getJedisClientConfiguration() {
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jedisPoolingClientConfigurationBuilder =
+                (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        // maximum number of free connections, default 8
+        genericObjectPoolConfig.setMaxIdle(maxIdle);
+        // Maximum number of connections Default 16
+        genericObjectPoolConfig.setMaxTotal(maxTotal);
+        // Get the maximum number of milliseconds to wait for the connection (BlockWhenExhausted if set to block), throw an exception if it times out, less than zero: block the indeterminate time,
+        // default -1
+        genericObjectPoolConfig.setMinIdle(minIdle);
+        return jedisPoolingClientConfigurationBuilder.poolConfig(genericObjectPoolConfig).build();
+    }
 
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
-        final RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration(redisHostname, port);
-        return new JedisConnectionFactory(standaloneConfig);
+        final RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration(redisHost, port);
+        if(StringUtils.isNotEmpty(redisPassword)){
+            standaloneConfig.setPassword(RedisPassword.of(redisPassword));
+        }
+        return new JedisConnectionFactory(standaloneConfig,getJedisClientConfiguration());
     }
 
 
@@ -41,8 +77,8 @@ public class AppConfig {
         return new JedisConnectionFactory(sentinelConfig);
     }*/
     @Bean
-    public RedisTemplate<Integer, Books> redisTemplate() {
-        final RedisTemplate<Integer, Books> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<Integer, Object> redisTemplate() {
+        final RedisTemplate<Integer, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
